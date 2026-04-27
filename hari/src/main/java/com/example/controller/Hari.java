a19c7ac516946ca299cfde714c4cf1e0f18a7b70
yagnatech_ma <ravi@yagna-tech.com>
2026-04-27 16:35:06 +0530

    initial commit

---
+++ hari/src/main/java/com/example/controller/Hari.java
@@ -0,0 +1,13 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hari {
+
+	@GetMapping("/wish")
+	public String wish() {
+		 return "hello boss how areyou...........!";
+	}
+}
